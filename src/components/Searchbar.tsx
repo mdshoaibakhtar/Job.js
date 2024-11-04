4c6fe44167745c663027a0c9c7de0795798cc206
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2024-11-04 08:42:32 +0530

    Update search box for mobile

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -12,9 +12,9 @@ const SearchBar: React.FC<ChildComponentProps> = ({ setSearchString }) => {
         }
     };
     return (
-        <div className="max-w-full flex justify-end px-10">
+        <div className="max-w-full flex justify-end px-4 sm:px-10">
             <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div className="relative w-1/4 transition-all duration-700">
+            <div className="relative w-full sm:w-1/4 transition-all duration-700">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
