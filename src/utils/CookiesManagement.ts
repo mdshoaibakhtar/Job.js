a8067e7e6185940f1d4fbe19d323b0df8877f6e4
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2025-01-23 20:14:59 +0530

    build fix

--- src/utils/CookiesManagement.ts
+++ src/utils/CookiesManagement.ts
@@ -9,8 +9,11 @@ export function clearCookie(name:string) {
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
 
-export function getCookie(name:string) {
-    const cookies = document.cookie.split(';');
+export function getCookie(name: string) {
+    let cookies: string[] = []
+    if (document !== undefined && document !== null && typeof document !== null && document.cookie.length !== 0) {
+        cookies = document.cookie.split(';');
+    }
     for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i].trim();
         if (cookie.startsWith(name + '=')) {
