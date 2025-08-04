004500438df2d93fefa819dc4ccdc04d61921228
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2025-08-04 23:36:35 +0530

    Fix prod url

--- src/variables/constant.ts
+++ src/variables/constant.ts
@@ -1,2 +1,2 @@
-// export const api_url = 'https://jobdotjs-apis.vercel.app/v1/api'
-export const api_url = 'http://127.0.0.1:8000/v1/api'
+export const api_url = 'https://jobdotjs-apis.vercel.app/v1/api'
+// export const api_url = 'http://127.0.0.1:8000/v1/api'
